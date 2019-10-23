f47f5b11000670489498de0f651a79928f3e87e0
Salim Alghoul <salemalghoul@gmail.com>
2019-10-23 21:31:00 +0100

    Level 2 & 3

--- app/src/main/java/com/example/myfirstapprollingdice/MainActivity.java
+++ app/src/main/java/com/example/myfirstapprollingdice/MainActivity.java
@@ -10,6 +10,8 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    int Counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +22,33 @@ public class MainActivity extends AppCompatActivity {
     {
 
         TextView TV = this.findViewById(R.id.textView);
+        TextView C = this.findViewById(R.id.textView3);
+        TextView I = this.findViewById(R.id.textView4);
 
         Random R = new Random();
         int Num = R.nextInt(6);
         TV.setText(Integer.toString(Num));
 
+        TextView Number = this.findViewById(com.example.myfirstapprollingdice.R.id.GNum);
+        String Value = Number.getText().toString();
+        int ValueS = Integer.parseInt(Value);
+
+
+
+        if(Num== ValueS)
+        {
+            Counter = Counter + 1;
+
+            C.setText("Congrats your guess was right " );
+            I.setText("Your Score Is : " + Counter);
+
+        }
+        else
+        {
+            C.setText(" Try Again");
+        }
+
     }
+
+
 }
