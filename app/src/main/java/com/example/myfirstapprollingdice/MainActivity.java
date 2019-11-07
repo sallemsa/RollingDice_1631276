9e94dfbf1260231f518b4a431fa9df230c2f986a
Salim Alghoul <salemalghoul@gmail.com>
2019-11-07 22:11:18 +0000

    Level 3a

--- app/src/main/java/com/example/myfirstapprollingdice/MainActivity.java
+++ app/src/main/java/com/example/myfirstapprollingdice/MainActivity.java
@@ -18,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public int Roll_the_dice()
+    {
+        Random R = new Random();
+        int Num = R.nextInt(7);
+        return Num;
+    }
+
     public void button_Click(View view)
     {
 
@@ -25,9 +32,8 @@ public class MainActivity extends AppCompatActivity {
         TextView C = this.findViewById(R.id.textView3);
         TextView I = this.findViewById(R.id.textView4);
 
-        Random R = new Random();
-        int Num = R.nextInt(6);
-        TV.setText(Integer.toString(Num));
+        int N = Roll_the_dice();
+        TV.setText(Integer.toString(N));
 
         TextView Number = this.findViewById(com.example.myfirstapprollingdice.R.id.GNum);
         String Value = Number.getText().toString();
@@ -35,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        if(Num== ValueS)
+        if(N== ValueS)
         {
             Counter = Counter + 1;
 
@@ -48,6 +54,45 @@ public class MainActivity extends AppCompatActivity {
             C.setText(" Try Again");
         }
 
+    }
+
+    public void Icebreak(View view)
+    {
+
+        int Dvalue = Roll_the_dice();
+        TextView TVr = this.findViewById(R.id.textView);
+        TVr.setText(Integer.toString(Dvalue));
+        TextView D = this.findViewById(R.id.textView2);
+
+        if(Dvalue==1)
+        {
+            D.setText("If you could go anywhere in the world, where would you go?");
+        }
+        else if(Dvalue==2)
+        {
+            D.setText("If you were stranded on a desert island, what three things would you want to take with you?");
+        }
+        else if(Dvalue==3)
+        {
+            D.setText("If you could eat only one food for the rest of your life, what would that be?");
+        }
+        else if(Dvalue==4)
+        {
+            D.setText("If you won a million dollars, what is the first thing you would buy?");
+        }
+        else if(Dvalue==5)
+        {
+            D.setText("If you could spaned the day with one fictional character, who would it be?");
+        }
+        else if(Dvalue==6)
+        {
+            D.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+        }
+        else
+        {
+            D.setText(" ");
+
+        }
     }
 
 
